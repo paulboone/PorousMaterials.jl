7f5340e0fb41504f89b68619d54e957bd51026ca
CorySimon <corysimon@berkeley.edu>
2018-06-21 15:37:31 -0700

    arni and cory meeting

--- src/EQeq.jl
+++ src/EQeq.jl
@@ -1,5 +1,9 @@
 using PorousMaterials
 using OffsetArrays
+using CSV
+using DataFrames
+
+# todo delete these constants
 const ϵ₀ = 4.7622424954949676e-7  # \epsilon_0 vacuum permittivity units: electron charge^2 /(A - K)
 const ϵᵣ = 1.67 # Global dielectric strength to prevent infinite charge separation
 #const K = 14.4
@@ -9,14 +13,46 @@ const kb = 8.6173303e-5 # eV / K
 
 """
 struct IonizationData
-    ionization_potential::Dict{Symbol, Array{Float64, 1}}
-    n_ionization::Dict{Symbol, Int64}
-    status::Dict{Symbol, AbstractString}
-    chargeCenter::Dict{Symbol, Int64}
+    """
+    Array of ionization potentials for the atoms (units: K).
+        entry i is ionization potential for atom i
+    """
+    ionization_potentials::Dict{Symbol, Array{Float64, 1}}
+    "Electron affinity (units: K)"
+    electron_affinity::Dict{Symbol, Float64}
+    """
+    Taylor expansion of energy of atom i is:
+        E_i(Q_i) = E_i(Q_i*) + ∂E_i (Q_i - Q_i*) ...
+        this holds Q_i^*, center about which Taylor expansion is performed.
+    """
+    charge_center::Dict{Symbol, Int64}
 end
 
-
 function read_ionization_data()
+    df = CSV.read("data/ionizationData.csv")
+    
+    ionization_potentials = Dict{Symbol, Array{Float64, 1}}()
+    electron_affinity = Dict{Symbol, Float64}()
+    charge_center = Dict{Symbol, Int64}()
+    
+    # TODO put units in column names of data.
+    for row in eachrow(df)
+        atom = Symbol(row[:atom])
+        charge_center[atom] = row[:chargeCenter]
+        electron_affinity[atom] = row[:Electron_affinity]
+        ionization_potentials[atom] = Float64[]
+        for i = 1:8
+            ie = row[Symbol("Ionization_energy_$i")]
+            if (ie == "na") | (ie == "np") # not possible/ not available?
+                break
+            end
+            push!(ionization_potentials[atom], parse(Float64, row[ie]))
+        end
+    end
+    return IonizationData(ionization_potentials, electron_affinity, charge_centers)
+end
+
+function old_read_ionization_data()
     # Data is read in as eV. This is changed to K by dividing by the Boltzmann constant, `kb`
     f = open("data/ionizationData.csv")
     lines = readlines(f)
@@ -63,14 +99,74 @@ function read_ionization_data()
     return IonizationData(ionization_potential, n_ionization, status, chargeCenter)
 end
 
-function compute_pure_J(ioData::IonizationData)
-    J = Dict{Symbol, Float64}()
-    for key in keys(ioData.ionization_potential)
-        # Why +2 and +1 again?...
-        J[key] = (ioData.ionization_potential[key][ioData.chargeCenter[key] + 2]
-                  - ioData.ionization_potential[key][ioData.chargeCenter[key] + 1])
+"""
+chemical hardness J:= = I₁ - A = ∂²E_i/∂Q_i², 
+where I₁ is ionization potential and A is electron affinity
+"""
+function chemical_hardness(ion_data::IonizationData, atom::Symbol)
+    return ion_data.ionization_potentials[atom][1] - ion_data.electron_affinity[atom]
+end
+
+"""
+electronegativity Χ := (I₁ + A) / 2
+where I₁ is ionization potential and A is electron affinity
+"""
+function electronegativity(ion_data::IonizationData, atom::Symbol)
+    return (ion_data.ionization_potentials[atom][1] + ion_data.electron_affinity[atom]) / 2.0
+end
+
+function ∂E_i∂_Q_i(ion_data::IonizationData, atom::Symbol)
+    charge_center = ion_data.charge_center[atom]
+    if charge_center == 0
+        return electronegativity(ion_data, atom)
+    else
+        return (ion_data.ionization_potentials[charge_center + 1] + ion_data.ionization_potentials[charge_center]) / 2.0
     end
-    return J
+end
+
+function ∂²E_i∂_Q_i²(ion_data::IonizationData, atom::Symbol)
+    charge_center = ion_data.charge_center[atom]
+    if charge_center == 0
+        return chemical_hardness(ion_data, atom)
+    else
+        return ion_data.ionization_potentials[charge_center + 1] - ion_data.ionization_potentials[charge_center]
+    end
+end
+
+"""
+Entries in matrix
+"""
+function compute_ϕ_ij(framework::Framework, i::Int, j::Int, ion_data::IonizationData; sr_cutoff::Float64=12.5)
+    rep_factors = replication_factors(framework, sr_cutoff_r)
+    eparams, kvecs, eikar, eikbr, eikcr = setup_Ewald_sum(sr_cutoff_r, framework.box)
+
+    # build molecules out of framework atoms in question.
+    x_i = framework.box.f_to_c * framework.xf[:, i]
+    x_j = framework.box.f_to_c * framework.xf[:, j]
+    atom_i = Molecule(:atom_i, 
+                      LennardJonesSphere[],
+                      PointCharge(1.0, x_i)
+                      x_i)
+    atom_j = Molecule(:atom_j, 
+                      LennardJonesSphere[],
+                      PointCharge(1.0, x_j)
+                      x_j)
+
+    ϕ = PorousMaterials.electrostatic_potential_energy([atom_i, atom_j], eparams, kvecs, eikar, eikbr, eikcr)
+
+    return ϕ.sr + ϕ.lr + ϕ.self * 2.0 + 
+
+ #     K = 1 / (4 * π * ϵ₀ * ϵᵣ)
+ #     J = compute_pure_J(ioData)
+ # 
+ #     α = compute_α(frame, repfactors, eparams, ind)
+ #     β = compute_β(frame, repfactors, eparams, eikar, eikbr, eikcr, ind, kvecs)
+ #     if ind[1] == ind[2]
+ #         return K * α - eparams.α * K / sqrt(π) + β + J[frame.atoms[ind[1]]]
+ #     else
+ #         E0 = compute_E0(frame, repfactors, J, ioData, ind)
+ #         return K / 2 * (α + E0) + β / (4 * π)
+ #     end
 end
 
 function assign_charges(frame::Framework, ioData::IonizationData, repfactors::Tuple{Int, Int, Int}, sr_cutoff_r::Float64, ϵ::Float64)
@@ -82,7 +178,6 @@ function assign_charges(frame::Framework, ioData::IonizationData, repfactors::Tu
     end
     return A_J
 end
-
 
 function compute_α(frame::Framework, repfactors::Tuple{Int, Int, Int}, eparams::EwaldParams, ind::Tuple{Int, Int})
     α = 0.0
@@ -142,20 +237,6 @@ function compute_E0(frame::Framework, repfactors::Tuple{Int, Int, Int}, pure_J::
     #Returns units of Å^(-1)
 end
 
-function compute_JPE(frame::Framework, ioData::IonizationData, repfactors::Tuple{Int, Int, Int}, sr_cutoff_r::Float64, ϵ::Float64, ind::Tuple{Int, Int})
-    eparams, kvecs, eikar, eikbr, eikcr = setup_Ewald_sum(sr_cutoff_r, frame.box)
-    K = 1 / (4 * π * ϵ₀ * ϵᵣ)
-    J = compute_pure_J(ioData)
-
-    α = compute_α(frame, repfactors, eparams, ind)
-    β = compute_β(frame, repfactors, eparams, eikar, eikbr, eikcr, ind, kvecs)
-    if ind[1] == ind[2]
-        return K * α - eparams.α * K / sqrt(π) + β + J[frame.atoms[ind[1]]]
-    else
-        E0 = compute_E0(frame, repfactors, J, ioData, ind)
-        return K / 2 * (α + E0) + β / (4 * π)
-    end
-end
 
 function compute_b_vector(frame::Framework, pure_J::Dict{Symbol, Float64}, ioData::IonizationData)
     b = zeros(frame.n_atoms)

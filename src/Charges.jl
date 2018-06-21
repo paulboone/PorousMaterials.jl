f767cd0872c0b15f6be48ecdfb665b8570eaa2e5
Arni Sturluson <sturlusa@oregonstate.edu>
2018-06-21 11:34:00 -0700

    Add newest version of Charges

--- src/Charges.jl
+++ src/Charges.jl
@@ -2,6 +2,7 @@ using PorousMaterials
 using OffsetArrays
 const ϵ₀ = 4.7622424954949676e-7  # \epsilon_0 vacuum permittivity units: electron charge^2 /(A - K)
 const ϵᵣ = 1.67 # Global dielectric strength to prevent infinite charge separation
+#const K = 14.4
 const R = 8.314 # J/ (mol K)
 const kb = 8.6173303e-5 # eV / K
 """
@@ -16,6 +17,7 @@ end
 
 
 function read_ionization_data()
+    # Data is read in as eV. This is changed to K by dividing by the Boltzmann constant, `kb`
     f = open("data/ionizationData.csv")
     lines = readlines(f)
     close(f)
@@ -26,11 +28,7 @@ function read_ionization_data()
     chargeCenter = Dict{Symbol, Int64}()
 
     for line in lines[2:end]
-        print(line)
-        @printf("\n")
         line = split(line, ",")
-        print(line)
-        @printf("\n")
         atom = Symbol(line[1])
         status[atom] = line[2]
 
@@ -40,6 +38,9 @@ function read_ionization_data()
             electron_affinity = 0.5 / kb
         else
             electron_affinity = parse(Float64, line[4]) / kb
+        end
+        if atom == :H
+            electron_affinity = -2 / kb
         end
 
         io_potential = tryparse(Float64, line[5])
@@ -65,8 +66,9 @@ end
 function compute_pure_J(ioData::IonizationData)
     J = Dict{Symbol, Float64}()
     for key in keys(ioData.ionization_potential)
+        # Why +2 and +1 again?...
         J[key] = (ioData.ionization_potential[key][ioData.chargeCenter[key] + 2]
-                  - ioData.ionization_potential[key][ioData.chargeCenter[key] + 1]) / kb
+                  - ioData.ionization_potential[key][ioData.chargeCenter[key] + 1])
     end
     return J
 end
@@ -97,6 +99,7 @@ function compute_α(frame::Framework, repfactors::Tuple{Int, Int, Int}, eparams:
         end
     end
     return α
+    # Returns units of Å^(-1)
 end
 
 function compute_β(frame::Framework, repfactors::Tuple{Int, Int, Int}, eparams::EwaldParams, eikar::OffsetArray{Complex{Float64}}, eikbr::OffsetArray{Complex{Float64}}, eikcr::OffsetArray{Complex{Float64}}, ind::Tuple{Int, Int}, kvectors::Array{Kvector, 1})
@@ -114,15 +117,16 @@ function compute_β(frame::Framework, repfactors::Tuple{Int, Int, Int}, eparams:
         fill_eikr!(eikcr, k_dot_dx[3, 1], eparams.kreps[3], true)
 
         for kvector in kvectors
-            @unsafe @inbounds β += kvector.wt * real(eikar[kvector.ka] * eikbr[kvector.kb] * eikcr[kvector.kc])
+            @unsafe @inbounds β += kvector.wt * real(eikar[kvector.ka] * eikbr[kvector.kb] * eikcr[kvector.kc]) # [β] ≡ Å^3 * K * e^(-2)
         end
     end
-    return β * 4 * π / eparams.box.Ω # This has already been divided by ϵ₀, so we'll have to be careful when multiplying by K (according to the paper)
+    return β / eparams.box.Ω # This has already been divided by ϵ₀, so we'll have to be careful when multiplying by K (according to the paper)
+    # Returns units of K * e^(-2)
 end
 
 function compute_E0(frame::Framework, repfactors::Tuple{Int, Int, Int}, pure_J::Dict{Symbol, Float64}, ioData::IonizationData, ind::Tuple{Int, Int})
     E0 = 0.0
-    K = 1 / (4 * π * ϵ₀)
+    K = 1 / (4 * π * ϵ₀ * ϵᵣ)
     for ra = 0:(repfactors[1] - 1), rb = 0:(repfactors[2] - 1), rc = 0:(repfactors[3] - 1)
         @inbounds dxf = frame.xf[:, ind[1]] - (frame.xf[:, ind[2]] + [ra, rb, rc])
         if norm(dxf) == 0
@@ -132,20 +136,21 @@ function compute_E0(frame::Framework, repfactors::Tuple{Int, Int, Int}, pure_J::
         @inbounds dx = frame.box.f_to_c * dxf
         r = norm(dx)
         J = (pure_J[frame.atoms[ind[1]]] + pure_J[frame.atoms[ind[2]]]) / 2
-        E0 += exp(-(J * r / K)^2) * (J/K - J^2*r/K^2 - 1/r)
+        E0 += exp(-(J * r / K)^2) * (J/K - J^2*r/K^2 - 1/r) # [E0] ≡ Å^(-1)
     end
     return E0
+    #Returns units of Å^(-1)
 end
 
 function compute_JPE(frame::Framework, ioData::IonizationData, repfactors::Tuple{Int, Int, Int}, sr_cutoff_r::Float64, ϵ::Float64, ind::Tuple{Int, Int})
     eparams, kvecs, eikar, eikbr, eikcr = setup_Ewald_sum(sr_cutoff_r, frame.box)
-    K = 1 / (4 * π * ϵ₀)
+    K = 1 / (4 * π * ϵ₀ * ϵᵣ)
     J = compute_pure_J(ioData)
 
     α = compute_α(frame, repfactors, eparams, ind)
     β = compute_β(frame, repfactors, eparams, eikar, eikbr, eikcr, ind, kvecs)
     if ind[1] == ind[2]
-        return K * α - eparams.α * K / sqrt(π) + β / (4 * π) + J[frame.atoms[ind[1]]]
+        return K * α - eparams.α * K / sqrt(π) + β + J[frame.atoms[ind[1]]]
     else
         E0 = compute_E0(frame, repfactors, J, ioData, ind)
         return K / 2 * (α + E0) + β / (4 * π)
@@ -157,7 +162,9 @@ function compute_b_vector(frame::Framework, pure_J::Dict{Symbol, Float64}, ioDat
     for i = 1:length(b)-1
         atom1 = frame.atoms[i]
         atom2 = frame.atoms[i+1]
-        b[i] = ioData.ionization_potential[atom2][1] - ioData.ionization_potential[atom1][1]
+        #ionization_potential[:Atom][1] is Electron Affinity
+        #ionization_potential[:Atom][2...] are 1st, 2nd, ... ionization energies
+        b[i] = ioData.ionization_potential[atom2][1] - ioData.ionization_potential[atom1][1] #- pure_J[atom2] * ioData.chargeCenter[atom2] + pure_J[atom1] * ioData.chargeCenter[atom1]
     end
     return b
 end
